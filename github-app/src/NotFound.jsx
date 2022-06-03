af4993f997b8c98d82ebf6d3136dbc2fb104638c
pablomarcel <pablomarcel@outlook.com>
2022-06-02 22:34:02 -0700

    Not Found Page

--- github-app/src/NotFound.jsx
+++ github-app/src/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NotFound(){
     return(
         <div>
-            <h1>404</h1>
+            <h1>404 Page</h1>
             <img src="https://media2.giphy.com/media/3o84TYaWr47oRcCQfu/giphy.gif?cid=790b7611e9f23169bf51db4678cb463198b3fbfcae942c11&rid=giphy.gif&ct=g" alt="Not Found"/>
         </div>
     )
